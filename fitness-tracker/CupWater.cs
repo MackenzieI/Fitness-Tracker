b6241aeeb2ef42d6c1bab12b302dca0763e638a5
Mackenzie <84205474+MackenzieI@users.noreply.github.com>
2026-06-22 12:38:09 -0500

    Implement water tracking
    Clicking on the button changes the amount of water shown in the label.

---
+++ fitness-tracker/CupWater.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class CupWater : TextureButton
+{
+	[Export] public NodePath LabelPath;
+	private Label water_label;
+	private int water_amount;
+	
+	public override void _Ready()
+	{
+		water_label = GetNode<Label>($"../../WaterLabel");
+		Pressed += ButtonPressed;
+	}
+	
+	private void ButtonPressed()
+	{
+		//A cup icon will fill with water. Currently using placeholder.
+		water_amount += 8;
+		water_label.Text = "Water today: " + water_amount;
+		
+		GD.Print("Water cup has been pressed.");
+	}
+}
