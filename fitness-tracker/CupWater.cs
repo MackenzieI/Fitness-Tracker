16f15507045c3c9bc20ca323a1a37c8f165aa199
Mackenzie <84205474+MackenzieI@users.noreply.github.com>
2026-06-25 23:28:04 -0500

    Update CupWater.cs
    Code currently commented out. Need to redo path for the label.

--- fitness-tracker/CupWater.cs
+++ fitness-tracker/CupWater.cs
@@ -3,13 +3,13 @@ using System;
 
 public partial class CupWater : TextureButton
 {
-	[Export] public NodePath LabelPath;
-	private Label water_label;
+	//[Export] public NodePath LabelPath;
+	//private Label water_label;
 	private int water_amount;
 	
 	public override void _Ready()
 	{
-		water_label = GetNode<Label>($"../../WaterLabel");
+		//water_label = GetNode<Label>($"../../WaterLabel");
 		Pressed += ButtonPressed;
 	}
 	
@@ -17,7 +17,7 @@ public partial class CupWater : TextureButton
 	{
 		//A cup icon will fill with water. Currently using placeholder.
 		water_amount += 8;
-		water_label.Text = "Water today: " + water_amount;
+		//water_label.Text = "Water today: " + water_amount;
 		
 		GD.Print("Water cup has been pressed.");
 	}
