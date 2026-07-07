5f1572083b9a3455d0c313014654656ee712f211
Mackenzie <84205474+MackenzieI@users.noreply.github.com>
2026-07-06 21:14:21 -0500

    Update CupWater.cs

--- fitness-tracker/CupWater.cs
+++ fitness-tracker/CupWater.cs
@@ -5,10 +5,12 @@ public partial class CupWater : TextureButton
 {
 	//[Export] public NodePath LabelPath;
 	//private Label water_label;
-	private int water_amount;
+	//private int water_amount;
 	
 	public override void _Ready()
 	{
+		//node = GetNode<Node>($"../../Node"); //Rename this later. Manages global variables.
+		
 		//water_label = GetNode<Label>($"../../WaterLabel");
 		Pressed += ButtonPressed;
 	}
@@ -16,8 +18,8 @@ public partial class CupWater : TextureButton
 	private void ButtonPressed()
 	{
 		//A cup icon will fill with water. Currently using placeholder.
-		water_amount += 8;
-		//water_label.Text = "Water today: " + water_amount;
+		//node.water_amount += 8;
+		//water_label.Text = "Water today: " + node.water_amount;
 		
 		GD.Print("Water cup has been pressed.");
 	}
