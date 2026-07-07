93c0ad1cfcf2c0a3de10c1335cc8f6355581df7a
Mackenzie <84205474+MackenzieI@users.noreply.github.com>
2026-07-06 21:11:50 -0500

    Add Node and code for global variables

---
+++ fitness-tracker/Node.cs
@@ -0,0 +1,10 @@
+//using Godot;
+//using System;
+
+//public partial class Node : Godot.Node
+//{
+//	[Export] public NodePath LabelPath;
+//	public Label water_label;
+//	public int water_amount;
+
+//}
